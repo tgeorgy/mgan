f3f0c1292f81d238408a03d9ae87f1f7f2338f62
tgeorgy <georgtrof@gmail.com>
2017-12-20 01:37:35 +0700

    changed generative model, a lot of small updates

--- models.py
+++ models.py
@@ -4,154 +4,107 @@ from torch.autograd import Variable
 import torch.nn.functional as F
 
 
+# Generaly G architecutre was bravely taken from this project with minor changes:
+# https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix
+
 class Generator(nn.Module):
-    def __init__(self, nlatent):
+    def __init__(self, input_nc=3, output_nc=4, ngf=64, norm_layer=nn.BatchNorm2d, use_dropout=False, n_blocks=6):
+        assert(n_blocks >= 0)
         super(Generator, self).__init__()
-        # Encoder layers
-        self.enc_conv1 = nn.Conv2d(3, 16, 3, 2, 1)
-        self.enc_bn1 = nn.BatchNorm2d(16)
-        self.enc_conv2 = nn.Conv2d(16, 32, 3, 2, 1)
-        self.enc_bn2 = nn.BatchNorm2d(32)
-        self.enc_conv3 = nn.Conv2d(32, 64, 3, 2, 1)
-        self.enc_bn3 = nn.BatchNorm2d(64)
-        self.enc_conv4 = nn.Conv2d(64, 128, 3, 2, 1)
-        self.enc_bn4 = nn.BatchNorm2d(128)
-        self.enc_conv5 = nn.Conv2d(128, 128, 3, 2, 1)
-        self.enc_bn5 = nn.BatchNorm2d(128)
+        self.input_nc = input_nc
+        self.output_nc = output_nc
+        self.ngf = ngf
 
-        # Latent parameters embedding layers
-        self.embed_fc1 = nn.Linear(nlatent, 128)
-        self.embed_bn1 = nn.BatchNorm1d(128)
-        self.embed_fc2 = nn.Linear(128, 64*4*4)
-        self.embed_bn2 = nn.BatchNorm2d(64)
+        model = [nn.ReflectionPad2d(3),
+                 nn.Conv2d(input_nc, ngf, kernel_size=7, padding=0),
+                 norm_layer(ngf),
+                 nn.ReLU(True)]
 
-        # Decoder layers
-        self.dec_conv1 = nn.Conv2d(128+64, 128, 3, 1, 1, bias=False)
-        self.dec_bn1 = nn.BatchNorm2d(128)
-        self.dec_conv1_ = nn.Conv2d(128, 128*4, 1, 1, 0, bias=False)
-        self.dec_bn1_ = nn.BatchNorm2d(128)
+        n_downsampling = 2
+        for i in range(n_downsampling):
+            mult = 2**i
+            model += [nn.Conv2d(ngf * mult, ngf * mult * 2, kernel_size=3,
+                                stride=2, padding=1),
+                      norm_layer(ngf * mult * 2),
+                      nn.ReLU(True)]
 
-        self.dec_conv2 = nn.Conv2d(128+3, 128, 3, 1, 1, bias=False)
-        self.dec_bn2 = nn.BatchNorm2d(128)
-        self.dec_conv2_ = nn.Conv2d(128, 128*4, 1, 1, 0, bias=False)
-        self.dec_bn2_ = nn.BatchNorm2d(128)
+        mult = 2**n_downsampling
+        for i in range(n_blocks):
+            model += [ResnetBlock(ngf * mult, norm_layer=norm_layer, use_dropout=use_dropout)]
 
-        self.dec_conv3 = nn.Conv2d(128+3, 64, 3, 1, 1, bias=False)
-        self.dec_bn3 = nn.BatchNorm2d(64)
-        self.dec_conv3_ = nn.Conv2d(64, 64*4, 1, 1, 0, bias=False)
-        self.dec_bn3_ = nn.BatchNorm2d(64)
+        for i in range(n_downsampling):
+            mult = 2**(n_downsampling - i)
+            model += [nn.ReflectionPad2d(1),
+                      nn.Conv2d(ngf * mult, int(ngf * mult / 2),
+                                kernel_size=3, stride=1),
+                      norm_layer(int(ngf * mult / 2)),
+                      nn.ReLU(True),
+                      nn.Conv2d(int(ngf * mult / 2), int(ngf * mult / 2)*4,
+                                kernel_size=1, stride=1),
+                      nn.PixelShuffle(2),
+                      norm_layer(int(ngf * mult / 2)),
+                      nn.ReLU(True),
+                     ]
+        model += [nn.ReflectionPad2d(3)]
+        model += [nn.Conv2d(ngf, output_nc, kernel_size=7, padding=0)]
 
-        self.dec_conv4 = nn.Conv2d(64+3, 64, 3, 1, 1, bias=False)
-        self.dec_bn4 = nn.BatchNorm2d(64)
-        self.dec_conv4_ = nn.Conv2d(64, 64*4, 1, 1, 0, bias=False)
-        self.dec_bn4_ = nn.BatchNorm2d(64)
+        self.model = nn.Sequential(*model)
 
-        self.dec_conv5 = nn.Conv2d(64, 32, 3, 1, 1, bias=False)
-        self.dec_conv5_ = nn.Conv2d(32, 3*4, 1, 1, 0, bias=False)
+    def forward(self, input):
+        output = self.model(input)
+        mask = F.sigmoid(output[:, :1])
+        oimg = output[:, 1:]
+        mask = mask.repeat(1, 3, 1, 1)
+        oimg = oimg*mask + input*(1-mask)
 
-        # Gate layers
-        self.gate_conv5 = nn.Conv2d(64, 32, 3, 1, 1, bias=False)
-        self.gate_conv5_ = nn.Conv2d(32, 4, 1, 1, 0, bias=False)
-        self.gate_bn5 = nn.BatchNorm2d(32)
+        return oimg, mask
 
-    def forward(self, img, embed):
-        enc = F.leaky_relu(self.enc_bn1(self.enc_conv1(img)), 0.2)
-        enc = F.leaky_relu(self.enc_bn2(self.enc_conv2(enc)), 0.2)
-        enc = F.leaky_relu(self.enc_bn3(self.enc_conv3(enc)), 0.2)
-        enc = F.leaky_relu(self.enc_bn4(self.enc_conv4(enc)), 0.2)
-        enc = self.enc_bn5(self.enc_conv5(enc))
+class ResnetBlock(nn.Module):
+    def __init__(self, dim, norm_layer, use_dropout):
+        super(ResnetBlock, self).__init__()
+        self.conv_block = self.build_conv_block(dim, norm_layer, use_dropout)
 
-        net_emb = F.leaky_relu(self.embed_bn1(self.embed_fc1(embed)), 0.2)
-        net_emb = self.embed_fc2(net_emb)
-        net_emb = net_emb.view(-1, 64, 4, 4)
-        net_emb = self.embed_bn2(net_emb)
+    def build_conv_block(self, dim, norm_layer, use_dropout):
+        conv_block = [nn.ReflectionPad2d(1),
+                       nn.Conv2d(dim, dim, kernel_size=3),
+                       norm_layer(dim),
+                       nn.ReLU(True)]
+        if use_dropout:
+            conv_block += [nn.Dropout(0.5)]
 
-        x = torch.cat([enc, net_emb], 1)
-        x = F.leaky_relu(x, 0.2)
+        conv_block += [nn.ReflectionPad2d(1),
+                       nn.Conv2d(dim, dim, kernel_size=3),
+                       norm_layer(dim)]
 
-        dec = F.leaky_relu(self.dec_bn1(self.dec_conv1(x)), 0.2)
-        dec = F.pixel_shuffle(self.dec_conv1_(dec), 2)
-        dec = F.leaky_relu(self.dec_bn1_(dec), 0.2)
+        return nn.Sequential(*conv_block)
 
-        dec = torch.cat([dec, F.avg_pool2d(img, 16)], 1)  # skip connection
-        dec = F.leaky_relu(self.dec_bn2(self.dec_conv2(dec)), 0.2)
-        dec = F.pixel_shuffle(self.dec_conv2_(dec), 2)
-        dec = F.leaky_relu(self.dec_bn2_(dec), 0.2)
-
-        dec = torch.cat([dec, F.avg_pool2d(img, 8)], 1)  # skip connection
-        dec = F.leaky_relu(self.dec_bn3(self.dec_conv3(dec)), 0.2)
-        dec = F.pixel_shuffle(self.dec_conv3_(dec), 2)
-        dec = F.leaky_relu(self.dec_bn3_(dec), 0.2)
-
-        dec = torch.cat([dec, F.avg_pool2d(img, 4)], 1)  # skip connection
-        dec = F.leaky_relu(self.dec_bn4(self.dec_conv4(dec)), 0.2)
-        dec = F.pixel_shuffle(self.dec_conv4_(dec), 2)
-        dec = F.leaky_relu(self.dec_bn4_(dec), 0.2)
-
-        gate = F.leaky_relu(self.gate_bn5(self.gate_conv5(dec)), 0.2)
-        gate = F.pixel_shuffle(self.gate_conv5_(gate), 2)
-        gate = gate.repeat(1, 3, 1, 1)
-
-        dec = F.leaky_relu(self.dec_conv5(dec), 0.2)
-        dec = F.pixel_shuffle(self.dec_conv5_(dec), 2)
-
-        gate = F.sigmoid(gate)*0.98 + 0.01  # Adding leak
-
-        dec = gate*dec + (1-gate)*img
-        dec = torch.clamp(dec, -1, 1)  # just in case
-
-        return dec, gate
+    def forward(self, x):
+        out = x + self.conv_block(x)
+        return out
 
 
 class Discriminator(nn.Module):
     def __init__(self):
         super(Discriminator, self).__init__()
-        # Encoder layers
-        self.enc_conv1 = nn.Conv2d(3, 16, 4, 2, 1, bias=False)
-        self.enc_bn1 = nn.BatchNorm2d(16)  # 64x64
-        self.enc_conv2 = nn.Conv2d(16, 32, 4, 2, 1, bias=False)
-        self.enc_bn2 = nn.BatchNorm2d(32)  # 32x64
-        self.enc_conv3 = nn.Conv2d(32, 64, 4, 2, 1, bias=False)
-        self.enc_bn3 = nn.BatchNorm2d(64)  # 16x16
-        self.enc_conv4 = nn.Conv2d(64, 128, 4, 2, 1, bias=False)
-        self.enc_bn4 = nn.BatchNorm2d(128)  # 8x8
-        self.enc_conv5 = nn.Conv2d(128, 256, 4, 2, 1, bias=False)
-        self.enc_bn5 = nn.BatchNorm2d(256)  # 4x4
-        self.enc_conv6 = nn.Conv2d(256, 256, 4, 2, 1, bias=False)
-        self.enc_bn6 = nn.BatchNorm2d(256)  # 2x2
-
-        self.fc = nn.Linear(1024, 1024)
-        self.bn = nn.BatchNorm1d(1024)
-
-        self.dec_conv6 = nn.ConvTranspose2d(256, 256, 4, 2, 1, bias=False)
-        self.dec_bn6 = nn.BatchNorm2d(256)
-        self.dec_conv5 = nn.ConvTranspose2d(256, 128, 4, 2, 1, bias=False)
-        self.dec_bn5 = nn.BatchNorm2d(128)
-        self.dec_conv4 = nn.ConvTranspose2d(128, 64, 4, 2, 1, bias=False)
-        self.dec_bn4 = nn.BatchNorm2d(64)
-        self.dec_conv3 = nn.ConvTranspose2d(64, 32, 4, 2, 1, bias=False)
-        self.dec_bn3 = nn.BatchNorm2d(32)
-        self.dec_conv2 = nn.ConvTranspose2d(32, 16, 4, 2, 1, bias=False)
-        self.dec_bn2 = nn.BatchNorm2d(16)
-        self.dec_conv1 = nn.ConvTranspose2d(16, 3, 4, 2, 1, bias=False)
+        self.conv_tower = nn.Sequential(
+            nn.Conv2d(3,   64,  4, 2),
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(),
+            nn.Conv2d(64,  128,  4, 2),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(),
+            nn.Conv2d(128,  256,  4, 2),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(),
+            nn.Conv2d(256,  512, 4, 2),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(),
+            nn.Conv2d(512, 512, 4),
+            nn.LeakyReLU(),
+            nn.AdaptiveAvgPool2d((1, 1)),
+            nn.Conv2d(512, 1, 1),
+        )
 
     def forward(self, img):
-        x = F.leaky_relu(self.enc_bn1(self.enc_conv1(img)), 0.2)
-        x = F.leaky_relu(self.enc_bn2(self.enc_conv2(x)), 0.2)
-        x = F.leaky_relu(self.enc_bn3(self.enc_conv3(x)), 0.2)
-        x = F.leaky_relu(self.enc_bn4(self.enc_conv4(x)), 0.2)
-        x = F.leaky_relu(self.enc_bn5(self.enc_conv5(x)), 0.2)
-        x = F.leaky_relu(self.enc_bn6(self.enc_conv6(x)), 0.2)
-
-        x = x.view(-1, 256*2*2)
-        x = F.leaky_relu(self.bn(self.fc(x)), 0.2)
-        x = x.view(-1, 256, 2, 2)
-
-        x = F.leaky_relu(self.dec_bn6(self.dec_conv6(x)), 0.2)
-        x = F.leaky_relu(self.dec_bn5(self.dec_conv5(x)), 0.2)
-        x = F.leaky_relu(self.dec_bn4(self.dec_conv4(x)), 0.2)
-        x = F.leaky_relu(self.dec_bn3(self.dec_conv3(x)), 0.2)
-        x = F.leaky_relu(self.dec_bn2(self.dec_conv2(x)), 0.2)
-        x = self.dec_conv1(x)
-
-        return x
+        output = self.conv_tower(img)
+        return output
--- train.py
+++ train.py
@@ -1,3 +1,5 @@
+import itertools
+
 import numpy as np
 import torch
 import torch.nn as nn
@@ -5,99 +7,163 @@ import torch.backends.cudnn as cudnn
 from torch.autograd import Variable
 import torch.optim as optim
 from torchvision.utils import save_image
+import torchvision
 
 from models import Generator, Discriminator
-from utils import CelebADatasetLoader
 
-
-cudnn.benchmark = True
-batch_size = 64
-n_latent = 16
-odir = 'ckpt'
-torch.manual_seed(1)
-np.random.seed(1)
-
-print('Initializing models')
-netG = Generator(n_latent).cuda()
-netD = Discriminator().cuda()
-
-criterion_MSE = nn.MSELoss().cuda()
-criterion_L1 = nn.L1Loss().cuda()
-
-# Init tensors
-inputG_gpu = torch.FloatTensor(batch_size, 3, 128, 128).cuda()
-inputD_gpu = torch.FloatTensor(batch_size, 3, 128, 128).cuda()
-latent_gpu = torch.FloatTensor(batch_size, n_latent).cuda()
-
-# Convert to Variables
-inputG_gpu = Variable(inputG_gpu)
-inputD_gpu = Variable(inputD_gpu)
-latent_gpu = Variable(latent_gpu)
-
-print('Loading data')
-dl = CelebADatasetLoader(batch_size, n_latent)
-
-optimizerD = optim.Adam(netD.parameters(), lr=2e-4, betas=(0.5, 0.999))
-optimizerG = optim.Adam(netG.parameters(), lr=2e-4, betas=(0.5, 0.999))
+batch_size = 1
+lambda_cycle = 2
+lambda_identity = 4
+lr = 0.0001
 
 print_every = 200
 n_epochs = 60
-margin = 0.015
-AE_coef = 0.15
+input_shape = (216, 176)
+odir = 'ckpt'
 
-netD.train()
-netG.train()
+torch.manual_seed(1)
+np.random.seed(1)
+
+cudnn.benchmark = True
+
+# Init dataset
+transformer = torchvision.transforms.Compose([
+        torchvision.transforms.RandomHorizontalFlip(),
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Lambda(
+            lambda img: img[:, 1:-1, 1:-1]),
+        torchvision.transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+    ])
+
+dataset = torchvision.datasets.ImageFolder('data/celeba/', transformer)
+
+labels_neg = [i for i, (_, l) in enumerate(dataset.imgs) if l == 0]
+labels_pos = [i for i, (_, l) in enumerate(dataset.imgs) if l == 1]
+
+sampler_neg = torch.utils.data.sampler.SubsetRandomSampler(labels_neg)
+sampler_pos = torch.utils.data.sampler.SubsetRandomSampler(labels_pos)
+
+pos_loader = torch.utils.data.DataLoader(dataset,
+                                         sampler=sampler_pos,
+                                         batch_size=batch_size,
+                                        )
+
+neg_loader = torch.utils.data.DataLoader(dataset,
+                                         sampler=sampler_neg,
+                                         batch_size=batch_size,
+                                        )
+
+# Init models
+netDP = Discriminator().cuda()
+netDN = Discriminator().cuda()
+netP2N = Generator().cuda()
+netN2P = Generator().cuda()
+
+criterion_cycle = nn.L1Loss()
+criterion_identity = nn.L1Loss()
+criterion_gan = nn.MSELoss()
+
+# Init tensors
+real_pos = torch.zeros(batch_size, 3,
+                       input_shape[0], input_shape[1]
+                      ).cuda()
+real_neg = torch.zeros(batch_size, 3,
+                       input_shape[0], input_shape[1]
+                      ).cuda()
+
+real_lbl = torch.zeros(batch_size, 1).cuda()
+real_lbl[:, 0] = 1
+fake_lbl = torch.zeros(batch_size, 1).cuda()
+fake_lbl[:, 0] = -1
+
+opt_G = optim.Adam(list(netP2N.parameters())+list(netN2P.parameters()), lr=lr, betas = (0.5, 0.999))
+opt_D = optim.Adam(list(netDN.parameters())+list(netDP.parameters()), lr=lr, betas = (0.5, 0.999))
+
+netDN.train()
+netDP.train()
+netP2N.train()
+netN2P.train()
 
 print('Training...')
-for epoch_i in xrange(n_epochs):
-    batch_i = 0
-    for inputG, inputD, latent in dl:
-        inputG_gpu.data.copy_(inputG)
-        inputD_gpu.data.copy_(inputD)
-        latent_gpu.data.copy_(latent)
+for epoch in xrange(n_epochs):
+    batch = 0
+    for (pos, _), (neg, _) in itertools.izip(pos_loader, neg_loader):
+        netDN.zero_grad()
+        netDP.zero_grad()
+        netP2N.zero_grad()
+        netN2P.zero_grad()
 
-        # Train Discriminator
-        netD.zero_grad()
-        netG.zero_grad()
+        real_pos.copy_(pos)
+        real_neg.copy_(neg)
 
-        output = netD(inputD_gpu)
-        loss_D_real = criterion_MSE(output, inputD_gpu)
-        loss_D_real.backward(retain_variables=True)
+        # Train P2N Generator
+        real_pos_v = Variable(real_pos)
+        fake_neg, mask_neg = netP2N(real_pos_v)
+        rec_pos, _ = netN2P(fake_neg)
+        fake_neg_lbl = netDN(fake_neg)
 
-        fake, mask = netG(inputG_gpu, latent_gpu)
-        output = netD(fake.detach())
-        loss_D_fake = margin - criterion_MSE(output, fake.detach())
-        loss_D_fake = loss_D_fake.clamp(min=0)
-        loss_D_fake.backward()
+        loss_P2N_cyc = criterion_cycle(rec_pos, real_pos_v)
+        loss_P2N_gan = criterion_gan(fake_neg_lbl, Variable(real_lbl))
+        loss_N2P_idnt = criterion_identity(fake_neg, real_pos_v)
 
-        loss_D = (loss_D_real + loss_D_fake) / 2
-        optimizerD.step()
+        # Train N2P Generator
+        real_neg_v = Variable(real_neg)
+        fake_pos, mask_pos = netN2P(real_neg_v)
+        rec_neg, _ = netP2N(fake_pos)
+        fake_pos_lbl = netDP(fake_pos)
 
-        # Train Generator
-        output = netD(fake)
-        loss_G = (output - fake).pow(2).mean()  # MSE
+        loss_N2P_cyc = criterion_cycle(rec_neg, real_neg_v)
+        loss_N2P_gan = criterion_gan(fake_pos_lbl, Variable(real_lbl))
+        loss_P2N_idnt = criterion_identity(fake_pos, real_neg_v)
 
-        loss_AE = criterion_L1(fake, inputG_gpu)
+        loss_G = ((loss_P2N_gan + loss_N2P_gan)*0.5 +
+                  (loss_P2N_cyc + loss_N2P_cyc)*0.5*lambda_cycle +
+                  (loss_P2N_idnt + loss_N2P_idnt)*0.5*lambda_identity)
 
-        loss_G = loss_G + AE_coef*loss_AE
         loss_G.backward()
+        opt_G.step()
 
-        optimizerG.step()
+        # Train Discriminators
+        netDN.zero_grad()
+        netDP.zero_grad()
+        fake_neg_score = netDN(fake_neg.detach())
+        loss_D = criterion_gan(fake_neg_score, Variable(fake_lbl))
+        fake_pos_score = netDP(fake_pos.detach())
+        loss_D += criterion_gan(fake_pos_score, Variable(fake_lbl))
 
-        batch_i += 1
-        if batch_i % print_every == 0 and batch_i > 1:
-            print('Epoch #%d' % (epoch_i+1))
-            print('Batch #%d' % batch_i)
+        real_neg_score = netDN.forward(real_neg_v)
+        loss_D += criterion_gan(real_neg_score, Variable(real_lbl))
+        real_pos_score = netDP.forward(real_pos_v)
+        loss_D += criterion_gan(real_pos_score, Variable(real_lbl))
+
+        loss_D = loss_D*0.25
+
+        loss_D.backward()
+        opt_D.step()
+
+        if batch % print_every == 0 and batch > 1:
+            print('Epoch #%d' % (epoch+1))
+            print('Batch #%d' % batch)
             print('Loss D: %0.3f' % loss_D.data[0] + '\t' +
                   'Loss G: %0.3f' % loss_G.data[0])
-            print('Loss D real: %0.3f' % loss_D_real.data[0] + '\t' +
-                  'Loss D fake: %0.3f' % loss_D_fake.data[0])
+            print('Loss P2N G real: %0.3f' % loss_P2N_gan.data[0] + '\t' +
+                  'Loss N2P G fake: %0.3f' % loss_N2P_gan.data[0])
 
-            print('Loss AE: %0.3f' % loss_AE.data[0])
             print('-'*50)
 
-            save_image(torch.cat([fake.data.cpu()[:8], inputG[:8]]),
-                       'progress.png', nrow=8, padding=1)
+            save_image(torch.cat([
+                real_neg.cpu()[0],
+                mask_pos.data.cpu()[0],
+                fake_pos.data.cpu()[0]], 2),
+                'progress_pos.png')
+            save_image(torch.cat([
+                real_pos.cpu()[0],
+                mask_neg.data.cpu()[0],
+                fake_neg.data.cpu()[0]], 2),
+                'progress_neg.png')
 
-            torch.save(netG, odir+'/netG.pth')
-            torch.save(netD, odir+'/netD.pth')
+            torch.save(netN2P, odir+'/netN2P.pth')
+            torch.save(netP2N, odir+'/netP2N.pth')
+            torch.save(netDN, odir+'/netDN.pth')
+            torch.save(netDP, odir+'/netDP.pth')
+        batch += 1
