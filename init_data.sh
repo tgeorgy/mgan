a3524013647fc5f4b2d4d081c36a1f7f501ec64f
tgeorgy <GeorgTrof@gmail.com>
2018-01-05 21:12:08 +0700

    data directory init

--- init_data.sh
+++ init_data.sh
@@ -1,3 +1,8 @@
+mkdir -p data
+mkdir -p data/celeba
+mkdir -p data/celeba/pos
+mkdir -p data/celeba/neg
+
 if [ ! -f data/img_align_celeba.zip ]; then
     echo "img_align_celeba.zip not found!"
 fi
